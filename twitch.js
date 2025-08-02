0eb3c37e02f3dc0bf7635312133b3f04238951e3
Yaro <osyaroslav@gmail.com>
2025-08-02 17:56:30 +0300

    update budges

--- twitch.js
+++ twitch.js
@@ -74,9 +74,9 @@ export function handleMessage(message) {
 function createBadgeIcons(badgesStr) {
   if (!badgesStr || typeof badgesStr !== 'string') return '';
   const badges = {
-    'broadcaster': `<svg class="badge" style="color: #e91916;" fill="currentColor" viewBox="0 0 20 20"><path d="M10 2a1 1 0 011 1v1.334a3.334 3.334 0 013.334 3.333h1.333a1 1 0 110 2h-1.333a3.334 3.334 0 01-3.334 3.333V17a1 1 0 11-2 0v-5.333A3.334 3.334 0 016.666 8.333H5.333a1 1 0 110-2h1.333A3.334 3.334 0 0110 3.001V3a1 1 0 011-1z"></path></svg>`,
-    'vip': `<svg class="badge" style="color: #e00ac1;" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>`,
-    'moderator': `<svg class="badge" style="color: #34d399;" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 18.5c5.166 0 9.666-3.583 11.334-8.5A11.954 11.954 0 0010 1.5c-5.166 0-9.666 3.583-11.334 8.5zM10 15a5 5 0 100-10 5 5 0 000 10z" clip-rule="evenodd"></path></svg>`,
+    'broadcaster': `<svg class="badge" style="color: #ef4444;" fill="currentColor" viewBox="0 0 20 20"><path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path><path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.523 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd"></path></svg>`,
+    'vip': `<svg class="badge" style="color: #ec4899;" fill="currentColor" viewBox="0 0 20 20"><path d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>`,
+    'moderator': `<svg class="badge" style="color: #22c55e;" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd"></path></svg>`,
     'subscriber': `<svg class="badge" style="color: #f59e0b;" fill="currentColor" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>`,
   };
   let html = '';
